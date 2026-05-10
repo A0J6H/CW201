c7c2e02cad4d15f5fb49503974b4f85445d3b14e
pierce <piercejgorman@gmail.com>
2026-05-10 18:16:48 +0100

    Adjusted book club format, added route to get user's bookclubs & to create a post. Basic club data loads into page.

--- client/js/bookclub.js
+++ client/js/bookclub.js
@@ -1,5 +1,7 @@
 
-let currentBookClubId = null;
+let clubData = null;
+
+
 // --- displays relevant user details if user logged in + log out option ---
 
 document.addEventListener("DOMContentLoaded", async () => {
@@ -19,26 +21,50 @@ document.addEventListener("DOMContentLoaded", async () => {
         // send a request for profile, includes the JWT in the header
         // route is: profile.js -> authmiddleware.js -> auth.js if successful
         // because we're using refresh tokens, we need a fetchWithAuth instead of fetch
-        const res = await fetchWithAuth("http://localhost:5000/auth/bookclubs", {
+        const res = await fetchWithAuth("http://localhost:5000/api/bookclubs/userclubs", {
         method: "GET"
         });
 
         const data = await res.json();
+        
 
         if (res.ok) {
             console.log("Club fetch successful:", data);
+            if (data.length > 0) {
+                let ind = 0;
+                data.forEach((club, ind) => {
+                    let clubContainer = document.getElementById("sidebar");
+                    let clubElement = document.createElement("h3");
+                    clubElement.textContent = club.name;
+                    clubElement.addEventListener("click", () => {
+                        loadClubContent(clubData, ind);});
+
+                    clubContainer.appendChild(clubElement);
+                })
+            clubData = data; 
+
+            loadClubContent(clubData, 0);
         }
         
+
         else {
             // if token invalid or expired, remove from local storage and go back to log in page
             console.log("Club fetch failed with status:", res.status);
         }
-    } catch (err) {
+    }
+}
+     catch (err) {
         // for general network errors, although this should be rarely triggered
         console.error(err);
     }
 });
 
+document.getElementById("chatBtn").addEventListener("click", () => {
+    mainContainer = document.getElementById("mainContentContainer");
+    document.getElementById("mainContentContainer").innerHTML = "";
+    
+
+});
 document.getElementById("redirectBtn").addEventListener("click", () => {
     window.location.href = "dashboard.html";
 });
@@ -57,3 +83,13 @@ document.getElementById("logoutBtn").addEventListener("click", async () => {
     console.log("pressing log out")
     window.location.href = "index.html";
 });
+
+function loadClubContent(clubData, index) {
+    cloneContainer = document.getElementById("cloneContainer");
+    let template = document.getElementById("bookClubTemplate");
+    cloneContainer.innerHTML = "";
+    const clone = template.content.cloneNode(true);
+    clone.getElementById("clubTitle").textContent = clubData[index].name;
+    clone.getElementById("clubDescription").textContent = clubData[index].description;
+    cloneContainer.appendChild(clone);
+}
--- server/routes/bookClubs.js
+++ server/routes/bookClubs.js
@@ -2,15 +2,69 @@ const express = require("express");
 const router = express.Router();
 const db = require("../db/db");
 
+//middleware for JWT handling:
+const verifyToken = require("../middleware/authmiddleware");
+const { ref } = require("node:process");
+
 router.get("/", (req, res) => {
   db.query("SELECT * FROM book_clubs", (err, results) => {
     if (err) {
       console.error(err);
       return res.status(500).json({ error: "DB query failed" });
     }
-
     res.json(results);
   });
 });
+
+router.get("/userclubs", verifyToken, async (req, res) => {
+  try {
+
+    db.query(
+      "SELECT members.userID, members.clubID, book_clubs.name, book_clubs.description FROM members JOIN book_clubs ON members.clubID = book_clubs.clubID WHERE members.userID = ?",
+      [req.user.userID],
+      (err, results) => {
+        if (err) return res.status(500).json({ error: "Failed to fetch users" });
+
+        if (results.length === 0) {
+          return res.status(404).json({ error: "Club not found" });
+        }
+
+        res.json(results);
+      }
+    );
+
+  } catch (err) {
+    res.status(500).json({ error: "Server error" });
+  }
+});
+
+router.post("/addPost", async (req,res) => {
+  const {clubID, userID, title, post} = req.body;
+
+  // validation (check if all fields filled & password typed correctly)
+  if (!clubID || !userID || !title || !post) {
+    return res.status(400).json({ error: "All fields are required" });
+  }
+
+  try {
+    // insert new post
+        db.query(
+          "INSERT INTO club_posts (clubID, userID, title, post) VALUES (?, ?, ?, ?)",
+          [clubID, userID, title, post],
+          (err, result) => {
+            if (err) return res.status(500).json({ error: "Failed to create post" });
+            res.status(201).json({ message: "Post successfully created" });
+          }
+
+        );
+
+    }
+    
+   catch (err){
+    res.status(500).json({ error: "Server error" });
+  }
+
+});
+
 
 module.exports = router;
